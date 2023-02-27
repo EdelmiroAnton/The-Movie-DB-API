07fa5b3c5f36c254d86e3b5b947e76193fec4b3c
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-02-26 21:25:57 -0300

    add favs to localStorage

--- src/App.js
+++ src/App.js
@@ -9,26 +9,43 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const favsItems = localStorage.getItem("favs");
+  let tempArrayOfMovies;
+
+  if (favsItems === null) {
+    tempArrayOfMovies = [];
+  } else {
+    tempArrayOfMovies = JSON.parse(favsItems);
+  }
 
   const addOrRemoveFavs = (e) => {
-    const btn = e.currentTarget
-    const parent = btn.parentElement
-    const srcImg = parent.querySelector("img").getAttribute("src")
-    const movieTitle = parent.querySelector("div").querySelector("div").textContent
-    const overview = parent.querySelector("div").querySelector("p").textContent
+    const btn = e.currentTarget;
+    const parent = btn.parentElement;
+    const srcImg = parent.querySelector("img").getAttribute("src");
+    const movieTitle = parent
+      .querySelector("div")
+      .querySelector("div").textContent;
+    const overview = parent.querySelector("div").querySelector("p").textContent;
     const movieData = {
-      srcImg, movieTitle, overview,
-      id: btn.dataset.movieId
-    }
-    console.log(movieData)
-  }
-  
+      srcImg,
+      movieTitle,
+      overview,
+      id: btn.dataset.movieId,
+    };
+    tempArrayOfMovies.push(movieData);
+    console.log(tempArrayOfMovies);
+    localStorage.setItem("favs", JSON.stringify(tempArrayOfMovies));
+  };
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/listado" element={<Listado addOrRemoveFavs={addOrRemoveFavs}/>} />
+        <Route
+          path="/listado"
+          element={<Listado addOrRemoveFavs={addOrRemoveFavs} />}
+        />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/detalle" element={<Detalle />} />
         <Route path="/resultados" element={<Resultados />} />
